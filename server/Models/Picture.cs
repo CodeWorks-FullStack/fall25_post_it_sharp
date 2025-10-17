95ea7292849a8c7271a536c6142edf9efe18bfc3
Jeremy Fowler <jembyfowler@gmail.com>
2025-10-17 09:35:16 -0600

    picture class

---
+++ server/Models/Picture.cs
@@ -0,0 +1,14 @@
+
+using System.ComponentModel.DataAnnotations;
+
+namespace post_it_sharp.Models;
+
+public class Picture : IRepoItem<int>
+{
+  public int Id { get; set; }
+  public DateTime CreatedAt { get; set; }
+  public DateTime UpdatedAt { get; set; }
+  public string CreatorId { get; set; }
+  public int AlbumId { get; set; }
+  [Url] public string ImgUrl { get; set; }
+}
